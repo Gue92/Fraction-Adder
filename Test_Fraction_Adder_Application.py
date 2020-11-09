6a68139846ac2844180d4eeec23e49495ce9e842
Günther.K <g.kaffenda@gmx.at>
2020-11-09 23:29:46 +0100

    Adapt the fraction adder test a second time

--- Test_Fraction_Adder_Application.py
+++ Test_Fraction_Adder_Application.py
@@ -1,9 +1,5 @@
 from core import add_frac
 
 def test_add_frac():
-	assert add_frac(1,7,6,7)
-		Zaehlerneu == 1
-
-def test_add_frac():
-	assert add_frac(1,7,6,7)		
-		Nennerneu == 1              
+	assert add_frac(1,7,6,7) == 1
+           
