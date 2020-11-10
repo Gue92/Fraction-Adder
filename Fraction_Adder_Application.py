0f64ae37d28e07587a490e82be8c6e410dba9d47
Günther.K <g.kaffenda@gmx.at>
2020-11-10 20:28:50 +0100

    Change the funtion syntax to access values for CI Application

--- Fraction_Adder_Application.py
+++ Fraction_Adder_Application.py
@@ -1,4 +1,3 @@
-
 def add_frac(Zaehler1,Nenner1,Zaehler2,Nenner2):
   
     Liste = [Zaehler1,Nenner1,Zaehler2,Nenner2]
@@ -15,13 +14,13 @@ def add_frac(Zaehler1,Nenner1,Zaehler2,Nenner2):
                 Counter = Counter + 1
         if Nenner1 > Nenner2 and Nenner1 % Nenner2 == 0:
             Faktor = int(Nenner1/Nenner2)  #  Zähler2 Anpassung
-            Zaehlerneu = int(Liste[0] + Liste[2] * Faktor)
-            Nennerneu = int(Nenner1)
+            add_frac.Zaehlerneu = int(Liste[0] + Liste[2] * Faktor)
+            add_frac.Nennerneu = int(Nenner1)
             
         if Nenner1 >Nenner2 and Nenner1 % Nenner2 == 0 and Counter!= 1:
             Faktor = int(Nenner1/Nenner2)  #  Addition wenn Nenner1 ist bereits Vielfaches
-            Zaehlerneu = int(Liste[0] * Counter + Liste[2] * Faktor)
-            Nennerneu = int(Nenner1)
+            add_frac.Zaehlerneu = int(Liste[0] * Counter + Liste[2] * Faktor)
+            add_frac.Nennerneu = int(Nenner1)
         
         
         if Nenner2 > Nenner1:  # Nenner2 Erweiterung auf Nenner1 - Vielfaches
@@ -30,53 +29,53 @@ def add_frac(Zaehler1,Nenner1,Zaehler2,Nenner2):
                 Counter = Counter + 1
         if Nenner2 > Nenner1 and Nenner2 % Nenner1 == 0 and Counter == 1:
             Faktor = int(Nenner2/Nenner1)  #  Zähler1 Anpassung
-            Zaehlerneu = int(Liste[0] * Faktor + Liste[2])
-            Nennerneu = int(Nenner2)
+            add_frac.Zaehlerneu = int(Liste[0] * Faktor + Liste[2])
+            add_frac.Nennerneu = int(Nenner2)
             
         if Nenner2 > Nenner1 and Nenner2 % Nenner1 == 0 and Counter!= 1:
             Faktor = Nenner2/Nenner1   #  Addition wenn Nenner1 ist bereits Vielfaches
-            Zaehlerneu = int(Liste[0] * Faktor + Liste[2] * Counter)
-            Nennerneu = int(Nenner2)
+            add_frac.Zaehlerneu = int(Liste[0] * Faktor + Liste[2] * Counter)
+            add_frac.Nennerneu = int(Nenner2)
             
         if Nenner2 == Nenner1:  #  Zähler-Addition wenn Nenner bereits gleich 
-            Zaehlerneu = int(Liste[0] + Liste[2])
-            Nennerneu = int(Nenner1)
+            add_frac.Zaehlerneu = int(Liste[0] + Liste[2])
+            add_frac.Nennerneu = int(Nenner1)
        
         # Ergebnis Vereinfachung
-        if Zaehlerneu == 0:
+        if add_frac.Zaehlerneu == 0:
             print("(",Zaehler1,"/" ,Nen1,") + (",Zaehler2,"/",Nen2,") = ",0)
         else:  
-            if Nennerneu != 0 and Zaehlerneu != 0:
+            if add_frac.Nennerneu != 0 and add_frac.Zaehlerneu != 0:
                    # Gemeinsamen Teiler finden ggt  
-                if Zaehlerneu>Nennerneu:
-                    for i in range(Nennerneu,1,-1 ):
+                if add_frac.Zaehlerneu>add_frac.Nennerneu:
+                    for i in range(add_frac.Nennerneu,1,-1 ):
                         #print("01=",i)
-                        z = Zaehlerneu % i
-                        z2 = Nennerneu % i
+                        z = add_frac.Zaehlerneu % i
+                        z2 = add_frac.Nennerneu % i
                         if z == 0 and z2 == 0:
                             ggt = i
                             #print("02=",ggt)
                             break    
-                if Zaehlerneu<Nennerneu:
-                    for i in range(Zaehlerneu,1,-1 ):
+                if add_frac.Zaehlerneu<add_frac.Nennerneu:
+                    for i in range(add_frac.Zaehlerneu,-1,1 ):##
                         #print("03=",i)
-                        z = Zaehlerneu % i
-                        z2 = Nennerneu % i
+                        z = add_frac.Zaehlerneu % i
+                        z2 = add_frac.Nennerneu % i
                         if z == 0 and z2 == 0:
                             ggt = i
                             #print("04",ggt)
                             break      
                 
                     #Zähler und Nenner kürzen
-            Z_short = int (Zaehlerneu/ggt)
-            N_short = int (Nennerneu/ggt)
+            add_frac.Z_short = int (add_frac.Zaehlerneu/ggt)
+            add_frac.N_short = int (add_frac.Nennerneu/ggt)
             
-            if N_short == 1:
-                print("(",Zaehler1,"/" ,Nen1,") + (",Zaehler2,"/",Nen2,") = ",Z_short)
+            if add_frac.N_short == 1:
+                print("(",Zaehler1,"/" ,Nen1,") + (",Zaehler2,"/",Nen2,") = ",add_frac.Z_short)
             
-            elif Z_short == N_short:
+            elif add_frac.Z_short == add_frac.N_short:
                 print("(",Zaehler1,"/" ,Nen1,") + (",Zaehler2,"/",Nen2,") = 1")
             
             else:
-                print("(",Zaehler1,"/" ,Nen1,") + (",Zaehler2,"/",Nen2,") = (",Z_short,"/",N_short,")" )
+                print("(",Zaehler1,"/" ,Nen1,") + (",Zaehler2,"/",Nen2,") = (",add_frac.Z_short,"/",add_frac.N_short,")" )
               
